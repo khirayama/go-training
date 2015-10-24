e6f686e7187588bc0404171fac1bdc4ff1f1cb24
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2015-10-24 14:47:16 +0900

    Using net/http to serve wiki pages

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"net/http"
 )
 
+// Data Structures
 type Page struct {
 	Title string
 	Body  []byte
@@ -26,10 +28,23 @@ func loadPage(title string) *Page {
 	return &Page{Title: title, Body: body}
 }
 
+// func main() {
+// 	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+// 	p1.save()
+// 	// p2, _ := loadPage("TestPage") ?
+// 	p2 := loadPage("TestPage")
+// 	fmt.Println(string(p2.Body))
+// }
+
+// Using net/http to serve wiki page
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	// p, _ := loadPage(title)
+	p := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
 func main() {
-	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-	p1.save()
-	// p2, _ := loadPage("TestPage") ?
-	p2 := loadPage("TestPage")
-	fmt.Println(string(p2.Body))
+	http.HandleFunc("/view/", viewHandler)
+	http.ListenAndServe(":8080", nil)
 }
