4ccb0d6b4859e285ae29025cb9a26e76f0cc9229
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2015-10-24 14:14:49 +0900

    Data Structures

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -1,6 +1,35 @@
 package main
 
 import (
-  "fmt"
-  "io/ioutil"
+	"fmt"
+	"io/ioutil"
 )
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) *Page {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		// return nil, err ?
+		return nil
+	}
+	// return &Page{Title: title, Body: body}[, nil ?
+	return &Page{Title: title, Body: body}
+}
+
+func main() {
+	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+	p1.save()
+	// p2, _ := loadPage("TestPage") ?
+	p2 := loadPage("TestPage")
+	fmt.Println(string(p2.Body))
+}
