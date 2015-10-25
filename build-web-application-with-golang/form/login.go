66db153299f18eb5adf37b63f28eed2c1144d871
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2015-10-25 09:11:12 +0900

    Add token

--- build-web-application-with-golang/form/login.go
+++ build-web-application-with-golang/form/login.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"crypto/md5"
 	"fmt"
 	"html/template"
+	"io"
 	"log"
 	"net/http"
+	"strconv"
 	"strings"
+	"time"
 )
 
 func sayHelloName(w http.ResponseWriter, r *http.Request) {
@@ -22,14 +26,27 @@ func sayHelloName(w http.ResponseWriter, r *http.Request) {
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
 	fmt.Println("method", r.Method)
 	if r.Method == "GET" {
+		crutime := time.Now().Unix()
+		h := md5.New()
+		io.WriteString(h, strconv.FormatInt(crutime, 10))
+		token := fmt.Sprintf("%x", h.Sum(nil))
+
 		t, _ := template.ParseFiles("login.gtpl")
-		t.Execute(w, nil)
+		t.Execute(w, token)
 	} else {
-		fmt.Println("username: ", r.Form["username"])
-		fmt.Println("password: ", r.Form["password"])
+		r.ParseForm()
+		token := r.Form.Get("token")
+		if token != "" {
+			// valid
+		} else {
+			// error
+		}
+		fmt.Println("username length: ", len(r.Form["username"][0]))
+		fmt.Println("username: ", template.HTMLEscapeString(r.Form.Get("username")))
+		fmt.Println("password: ", template.HTMLEscapeString(r.Form.Get("password")))
+		template.HTMLEscape(w, []byte(r.Form.Get("username")))
 	}
 }
 
--- build-web-application-with-golang/form/login.gtpl
+++ build-web-application-with-golang/form/login.gtpl
@@ -7,6 +7,7 @@
     ユーザ名:<input type="text" name="username">
     パスワード:<input type="password" name="password">
     <input type="submit" value="ログイン">
+    <input type="hidden" name="token" value="{{.}}">
 </form>
 </body>
 </html>
