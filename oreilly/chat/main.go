a7c17dfdfded22cb23fb3ba42ba5e671eb4d51cf
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-02-09 07:33:45 +0900

    Done 1.1.1

--- oreilly/chat/main.go
+++ oreilly/chat/main.go
@@ -3,21 +3,27 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-			<html>
-				<head>
-					<title>チャット</title>
-				</head>
-				<body>
-				チャットしましょう！
-				</body>
-			</html>
-		`))
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
+	t.templ.Execute(w, nil)
+}
+
+func main() {
+	// ルート
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 
 	// Webサーバーを開始します
 	if err := http.ListenAndServe(":8080", nil); err != nil {
