890613ee62f945602790ded17b65209798d25277
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-02-11 22:10:46 +0900

    Update room

--- oreilly/chat/room.go
+++ oreilly/chat/room.go
@@ -1,6 +1,41 @@
 package main
 
 type room struct {
-	// forwardは他のクライアントに’転送するためのメッセージを保持するチャネルです
+	// forwardは他のクライアントに転送するためのメッセージを保持するチャネルです
 	forward chan []byte
+	// joinはチャットルームに参加しようとしているクライアントのためのチャネルです。
+	join chan *client
+	// leaveはチャットルームから退室しようとしているクライアントのためのチャネルです。
+	leave chan *client
+	// clientsには在室しているすべてのクライアントが保持されます。
+	clients map[*client]bool
 }
+
+func (r *room) run() {
+	for { // 無限ループ -> goroutineとして実行するのであれば問題ない
+		select {
+		case client := <-r.join:
+			// 参加
+			r.clients[client] = true
+		case client := <-r.leave:
+			// 退室
+			delete(r.clients, client)
+			close(client.send)
+		case msg := <-r.forward:
+			// すべてのクライアントにメッセージを転送
+			for client := range r.clients {
+				select {
+				case client.send <- msg:
+					// メッセージを送信
+				default:
+					// 送信に失敗
+					delete(r.clients, client)
+					close(client.send)
+				}
+			}
+		}
+	}
+}
+
+// structの第三引数
+// <-
