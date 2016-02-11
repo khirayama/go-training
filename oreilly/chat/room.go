88c0a0c523863604676eb94c7e1305fcce1e9631
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-02-12 07:50:05 +0900

    Update room.go

--- oreilly/chat/room.go
+++ oreilly/chat/room.go
@@ -37,5 +37,28 @@ func (r *room) run() {
 	}
 }
 
+const (
+	socketBufferSize  = 1024
+	messageBufferSize = 256
+)
+
+var upgrader = &websocket.Upgrader{ReadBufferSize: socketBufferSize, WriteBufferSize: socketBufferSize}
+
+func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	socket, err := upgrader.Upgrade(w, req, nil)
+	if err != nil {
+		log.Fatal("ServeHTTP:", err)
+	}
+	client := &client{
+		socket: socket,
+		send:   make(chan []byte, messageBufferSize),
+		room:   r,
+	}
+	r.join <- client
+	defer func() { r.leave <- client }()
+	go client.write()
+	client.read()
+}
+
 // structの第三引数
 // <-
