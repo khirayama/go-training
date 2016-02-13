1069c9d3473167d7e3edefb100aa5f3799780fdf
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-02-13 14:10:05 +0900

    Settingable port

--- oreilly/chat/main.go
+++ oreilly/chat/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 	"path/filepath"
@@ -18,10 +19,13 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, nil)
+	t.templ.Execute(w, r)
 }
 
 func main() {
+	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
+	flag.Parse() // フラグを解釈します
+
 	r := newRoom()
 	// ルート
 	http.Handle("/", &templateHandler{filename: "chat.html"})
@@ -31,7 +35,8 @@ func main() {
 	go r.run()
 
 	// Webサーバーを開始します
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	log.Println("Webサーバーを開始します。ポート: ", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe", err)
 	}
 }
