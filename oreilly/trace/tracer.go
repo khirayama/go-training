43f45dba572b22755007688554daf8942c88e506
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-02-14 17:48:40 +0900

    Finish minimum tracer

--- oreilly/trace/tracer.go
+++ oreilly/trace/tracer.go
@@ -1,6 +1,24 @@
 package trace
 
+import (
+	"fmt"
+	"io"
+)
+
 // Tracerはコード内での出来事を記録できるオブジェクトを表すインターフェースです。
 type Tracer interface {
 	Trace(...interface{}) // Goコミュニティではこのような引数を何個でも受け取れる定義が多い
+}
+
+type tracer struct {
+	out io.Writer
+}
+
+func (t *tracer) Trace(a ...interface{}) {
+	t.out.Write([]byte(fmt.Sprint(a...)))
+	t.out.Write([]byte("\n"))
+}
+
+func New(w io.Writer) Tracer {
+	return &tracer{out: w}
 }
--- oreilly/trace/tracer_test.go
+++ oreilly/trace/tracer_test.go
@@ -1,11 +1,22 @@
 package trace
 
 import (
+	"bytes"
 	"testing"
 )
 
 // filenameは*_test.go
 // 名前がTestから始まり、*testing.T型の引数を1つ受け取る関数はユニットテストとみなされる
 func TestNew(t *testing.T) {
-	t.Error("まだテストを作成していません。")
+	var buf bytes.Buffer
+	tracer := New(&buf)
+
+	if tracer == nil {
+		t.Error("Newからの戻り値がnilです")
+	} else {
+		tracer.Trace("こんちには、traceパッケージ")
+		if buf.String() != "こんちには、traceパッケージ\n" {
+			t.Errorf("'%s'という誤った文字列が出力されました", buf.String())
+		}
+	}
 }
