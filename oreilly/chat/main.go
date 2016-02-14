7049126d6280711f6eb8bad5bf0c8ea110189054
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-02-15 07:25:46 +0900

    Show user name

--- oreilly/chat/main.go
+++ oreilly/chat/main.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/stretchr/gomniauth"
 	"github.com/stretchr/gomniauth/providers/google"
+	"github.com/stretchr/objx"
 
 	"../trace"
 )
@@ -25,7 +26,13 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, r)
+	data := map[string]interface{}{
+		"Host": r.Host,
+	}
+	if authCookie, err := r.Cookie("auth"); err == nil {
+		data["UserData"] = objx.MustFromBase64(authCookie.Value)
+	}
+	t.templ.Execute(w, data)
 }
 
 func main() {
