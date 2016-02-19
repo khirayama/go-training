aac7b75791cda87405689434e21ebe3d05c8a133
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-02-20 08:48:32 +0900

    Create logout

--- oreilly/chat/main.go
+++ oreilly/chat/main.go
@@ -54,6 +54,16 @@ func main() {
 	http.Handle("/login", &templateHandler{filename: "login.html"})
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
 	http.HandleFunc("/auth/", loginHandler)
+	http.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
+		http.SetCookie(w, &http.Cookie{
+			Name:   "auth",
+			Value:  "",
+			Path:   "/",
+			MaxAge: -1,
+		})
+		w.Header()["Location"] = []string{"/chat"}
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	})
 	http.Handle("/room", r)
 
 	// チャットルームを開始する
