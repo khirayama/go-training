e938f9cedf1848748733ebb09a46a506552a22a0
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-03-11 08:07:09 +0900

    Fix some typo and bugs

--- oreilly/chat/avatar_test.go
+++ oreilly/chat/avatar_test.go
@@ -8,7 +8,7 @@ func TestAuthAvatar(t *testing.T) {
 	var authAvatar AuthAvatar
 	client := new(client)
 	url, err := authAvatar.GetAvatarURL(client)
-	if err != ErrAvatarURL {
+	if err != ErrNoAvatarURL {
 		t.Error("値が存在しない場合、AuthAvatar.GetAvatarURLは" +
 			"ErrNoAvatarURLを返すべきです")
 	}
@@ -16,7 +16,7 @@ func TestAuthAvatar(t *testing.T) {
 	// 値をリセットする
 	testUrl := "http://url-to-avatar/"
 	client.userData = map[string]interface{}{"avatar_url": testUrl}
-	url, err authAvatar.GetAvatarURL(client)
+	url, err = authAvatar.GetAvatarURL(client)
 	if err != nil {
 		t.Error("値が存在しない場合、AuthAvatar.GetAvatarURLは" +
 			"エラーを返すべきではありません")
