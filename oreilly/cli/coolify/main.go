464bed601a3186d033f40a197da0b2af2218003b
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-04-19 09:04:27 +0900

    Create coolify

---
+++ oreilly/cli/coolify/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	s := bufio.NewScanner(os.Stdin)
+	for s.Scan() {
+		word := []byte(s.Text())
+		if randBool() {
+			var vI int = -1
+			for i, char := range word {
+				switch char {
+				case 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U':
+					if randBool() {
+						vI = i
+					}
+				}
+			}
+		}
+	}
+}
