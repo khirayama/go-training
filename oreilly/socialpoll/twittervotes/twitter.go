30bf62989b4105021acf05f83ee7ecd76f71f912
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-04-23 16:34:17 +0900

    Create twitter.go

---
+++ oreilly/socialpoll/twittervotes/twitter.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net"
+)
+
+var conn net.Conn
+
+func dial(netw, addr string) (net.Conn, error) {
+	if conn != nil {
+		conn.Close()
+		conn = nil
+	}
+	netc, err := net.DailTimeout(netw, addr, 5*time.Second)
+	if err != nil {
+		return nil, err
+	}
+	conn = netc
+	return netc, nil
+}
