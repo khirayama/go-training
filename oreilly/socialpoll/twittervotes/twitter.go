79277e38362db86a4e8e7ccd9ed42d104cd977ed
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-04-26 07:36:27 +0900

    Read env vars

--- oreilly/socialpoll/twittervotes/twitter.go
+++ oreilly/socialpoll/twittervotes/twitter.go
@@ -3,6 +3,9 @@ package main
 import (
 	"io"
 	"net"
+	"log"
+
+	"github.com/joeshaw/envdecode"
 )
 
 var conn net.Conn
@@ -28,5 +31,32 @@ func closeConn() {
 	}
 	if reader != nil {
 		reader.Close()
+	}
+}
+
+var (
+	authClient *oauth.Client
+	creds      *oauth.Credentials
+)
+
+func setupTwitterAuth() {
+	var ts struct {
+		ConsumertKey   string `env:"SP_TWITTER_KEY",required`
+		ConsumerSecret string `env:"SP_TWITTER_SECRET,required"`
+		AccessToken    string `env:SP_TWITTER_ACCESSTOKEN,required`
+		AccessSecret   string `env:SP_TWITTER_ACCESSSECRET,required`
+	}
+	if err := envdecode.Decode(&ts); err != nil {
+		log.Fatalln(err)
+	}
+	creds = &oauth.Credentials{
+		Token:  ts.AccessToken,
+		Secret: ts.AccessSecret,
+	}
+	authClient = &oauth.Client{
+		Credentials: oauth.Credentials{
+			Token: ts.ConsumertKey,
+			Secret: ts.ConsumerSecret,
+		}
 	}
 }
